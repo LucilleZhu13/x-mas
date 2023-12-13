c481fb1ebf5185f41507c9515f4b8929602605fa
Jiangyue Zhu <jzhu484@gatech.edu>
2023-12-13 13:50:21 -0500

    add files

---
+++ script.js
@@ -0,0 +1,17 @@
+const radioBtns = document.querySelectorAll('input[type="radio"]');
+var name;
+var number;
+var color;
+var thing;
+
+function clearForm() {
+    document.getElementById("name").value = "";
+    document.getElementById("colorform").reset()
+    document.getElementById("number").value = "";
+    document.getElementById("thingform").reset()
+    document.getElementById("likeform").reset()
+}
+
+function submitForm() {
+
+}
